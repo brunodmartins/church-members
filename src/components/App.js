47adec8789ff241cf754395f8b38967335eb6ed6
Bruno Damasceno Martins <bdm2943@gmail.com>
2018-05-25 18:51:33 -0300

    Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React, { Component} from "react";
 import "./App.css";
+import Axios from 'axios';
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      msg: "Pinggg..."
+    }
+  }
+
+  componentDidMount(){
+    Axios.get("http://disciples-api.herokuapp.com/ping")
+      .then(res => {
+        this.setState({
+          msg: "Pinged!"
+        });
+      })
+      .catch(err => {
+        this.setState({
+          msg: "Not pinged"
+        });
+      });
+  }
+
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> {this.state.msg} </h1>
       </div>
     );
   }
