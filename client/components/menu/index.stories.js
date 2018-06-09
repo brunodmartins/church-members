f93220b206c47fbddcf2b1d433691836ab580682
Bruno Damasceno Martins <bdm2943@gmail.com>
2018-06-08 21:41:58 -0300

    Ajustando storybook

--- client/components/menu/index.stories.js
+++ client/components/menu/index.stories.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Menu from './menu';
+import { BrowserRouter as Router } from "react-router-dom";
 
-storiesOf("Menu", module).add("default", () => <Menu />);
+
+storiesOf("Menu", module).add("default", () => {
+  return <Router>
+    <Menu />
+  </Router>;
+});
