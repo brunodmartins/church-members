a7de50843ba5fb4ae9a82debd6c5308944205b27
Bruno Damasceno Martins <bdm2943@gmail.com>
2018-06-21 21:47:31 -0300

    Adding auth0

--- client/components/app/App.js
+++ client/components/app/App.js
@@ -4,17 +4,41 @@ import Axios from "axios";
 import Menu from '../menu/menu';
 import MembersPanel from '../members/membersPanel/membersPanel';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Callback from "../callback/callback";
+import Home from "../home/home";
+
 
 class App extends Component {
+
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  handleAuthentication = (nextState, replace) => {
+    if (/access_token|id_token|error/.test(nextState.location.hash)) {
+      this.props.auth.handleAuthentication();
+    }
+  }
+  
+
 
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router history={this.props.history}>
           <div>
           <Menu/>
           <div className="container">
+            <Route exact path="/" render={(props) => <Home auth={this.props.auth} />} />
             <Route exact path="/membros" component={MembersPanel} />
+            <Route exact path="/callback_auth" render={(props) => {
+              this.handleAuthentication(props);
+              return <Callback {...props}/>
+            }} />
           </div>
           </div>
         </Router>
--- client/components/members/memberCard/memberCard.js
+++ client/components/members/memberCard/memberCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 require("./memberCard.css");
 
 const MemberCard = ({name, completeName}) => {
