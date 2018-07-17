b397926c7d57f2737dd9fb18cc6464203a598659
BrunoDM2943 <bdm2943@gmail.com>
2018-07-16 22:26:34 -0300

    Ajuste no menu

--- client/components/menu/menu.js
+++ client/components/menu/menu.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
 import MenuTitle from './menuTitle/menuTitle';
 import MenuItem from './menuItem/menuItem';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 require("./menu.css");
 
 class Menu extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      menuClass: "menu-itens",
+      menuOpen: false,
+    };
+  }
+
+  openMenu = (e) => {
+    console.log(e);
+    const menuClass = this.state.menuOpen ? "menu-itens" : "menu-itens-opened";
+    
+    this.setState({
+        menuClass,
+        menuOpen: !this.state.menuOpen
+    })
+  }
+
   render() {
     return (
       <div className="menu">
-        <div className="menu-itens">
           <MenuTitle/>
-          <MenuItem item="Membros" link="/membros" />
-          <MenuItem item="Relatorio" link="/relatorioMembros" />
+          <div className={this.state.menuClass}>
+          <MenuItem  item="Membros" link="/membros" />
+          <MenuItem  item="Relatorio" link="/relatorioMembros" />
         </div>
+        <div className="menu-icon" onClick={this.openMenu}>
+            <FontAwesomeIcon icon="bars" />
+          </div>
+
       </div>
     );
   }
