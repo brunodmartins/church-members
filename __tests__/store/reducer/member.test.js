db89fc1e6daf42cb54c9a1a8863f37e6fae8ef91
BrunoDM2943 <bdm2943@gmail.com>
2018-08-13 00:26:06 -0300

    Adding package json commands

--- __tests__/store/reducer/member.test.js
+++ __tests__/store/reducer/member.test.js
@@ -1,17 +1,17 @@
-import { member }  from '../../../client/reducers'
-describe("Member reducer", () => {
-    it("NAVIGATE_TOMEMBER sucess", () => {
-        const state = {}
-        const action = {
-            type: 'NAVIGATE_MEMBER',
-            member: {
-                id: "XPTO",
-            },
-        }
-        const results = member(state, action);
-        expect(results)
-            .toEqual({
-                id: "XPTO"
-            });
-    })
+import { member }  from '../../../client/reducers';
+describe('Member reducer', () => {
+	it('NAVIGATE_TOMEMBER sucess', () => {
+		const state = {};
+		const action = {
+			type: 'NAVIGATE_MEMBER',
+			member: {
+				id: 'XPTO',
+			},
+		};
+		const results = member(state, action);
+		expect(results)
+			.toEqual({
+				id: 'XPTO'
+			});
+	});
 });
