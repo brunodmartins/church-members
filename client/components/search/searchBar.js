662f2ec52252d2bd22abe777914b93bd3ae5a5bc
BrunoDM2943 <bdm2943@gmail.com>
2019-06-20 14:16:33 -0300

    New input component

--- client/components/search/searchBar.js
+++ client/components/search/searchBar.js
@@ -1,19 +1,25 @@
 import React, {useState} from 'react';
+import InputText from 'components/InputText';
 
 require("./search.css");
 
 const SearchBar = ({onSearch}) => {
     const [value, setValue] = useState('');
+
+    const searchAllowed = (value) => {
+      return value.length < 3;
+    }
+    
     return (
         <div className="div-search-bar">
-            <input type="text" placeholder="Pesquisar" value={value} onChange={(e) => {
-              if(e.which == 13 && value.length >= 3) {
-                onSearch(value);
-                return;
-              }
-              setValue(e.target.value);
-            }}/>
-            <button type="button" onClick={() => onSearch(value)} disabled={value.length < 3}>
+            <InputText
+              placeholder="Pesquisar"
+              value={value}
+              setValue={setValue}
+              onEnter={() => {
+                if(searchAllowed(value)) onSearch(value)
+              }}/>
+            <button type="button" onClick={() => onSearch(value)} disabled={searchAllowed(value)}>
               Pesquisar
             </button>
         </div>
