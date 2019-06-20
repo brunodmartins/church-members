fe3ef2ee61f6ef2dfd43b0b421df05bdf86ff3c6
BrunoDM2943 <bdm2943@gmail.com>
2019-06-20 14:50:11 -0300

    Fix css

--- client/components/search/searchBar.js
+++ client/components/search/searchBar.js
@@ -1,28 +1,26 @@
 import React, {useState} from 'react';
 import InputText from 'components/InputText';
-
-require("./search.css");
+import Button from 'components/button';
+require('./search.css');
 
 const SearchBar = ({onSearch}) => {
-    const [value, setValue] = useState('');
+	const [value, setValue] = useState('');
 
-    const searchAllowed = (value) => {
-      return value.length < 3;
-    }
-    
-    return (
-        <div className="div-search-bar">
-            <InputText
-              placeholder="Pesquisar"
-              value={value}
-              setValue={setValue}
-              onEnter={() => {
-                if(searchAllowed(value)) onSearch(value)
-              }}/>
-            <button type="button" onClick={() => onSearch(value)} disabled={searchAllowed(value)}>
-              Pesquisar
-            </button>
-        </div>
-  );
-}
+	const searchAllowed = (value) => {
+		return value.length < 3;
+	};
+
+	return (
+		<div className="div-search-bar">
+			<InputText
+				placeholder="Pesquisar"
+				value={value}
+				setValue={setValue}
+				onEnter={() => {
+					if(searchAllowed(value)) onSearch(value);
+				}}/>
+			<Button onClick={() => onSearch(value)} disabled={searchAllowed(value)} label='Pesquisar'/>
+		</div>
+	);
+};
 export default SearchBar;
