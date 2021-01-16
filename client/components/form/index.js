a3b5f452f9f0c45bee034747ed7fd1fc4f046655
Bruno <bdm2943@gmail.com>
2021-01-16 20:31:16 -0300

    Adding MaterialUI

--- client/components/form/index.js
+++
@@ -1,3 +0,0 @@
-import InputText from './inputText';
-import Form from './form';
-export {InputText, Form};
--- client/components/members/memberForm/memberForm.js
+++ client/components/members/memberForm/memberForm.js
@@ -1,38 +1,78 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Form, InputText} from 'components/form';
+import {TextField, FormControl, InputLabel, Select, Button, FormHelperText} from '@material-ui/core';
 
 class MemberForm extends React.Component {
 
 	constructor(props) {
 		super(props);
+		const error = {
+			firstName:'',
+			lastName:'',
+			gender:'',
+		};
 		if(props.member) {
-			this.state = {...props.member};
+			this.state = {...props.member, error};
 		} else {
-			this.state = {};
+			this.state = {error};
 		}
 		this.setInputTextValue = this.setInputTextValue.bind(this);
+		this.validate = this.validate.bind(this);
 	}
 
 	setInputTextValue(e) {
-		console.log(e.target.value);
 		const state = this.state;
 		state[e.target.id] = e.target.value;
 		this.setState(state);
 	}
 
+	validate(){
+		const error = this.state.error;
+		if(this.state.firstName === undefined || this.state.firstName === ''){
+			error.firstName = 'Nome precisa ser preenchido';
+		} else {
+			error.firstName = '';
+		}
+		if(this.state.lastName === undefined || this.state.lastName === ''){
+			error.lastName = 'Sobrenome precisa ser preenchido';
+		} else {
+			error.lastName = '';
+		}
+		if(this.state.gender === undefined || this.state.gender === ''){
+			error.gender = 'Sexo precisa ser preenchido';
+		} else {
+			error.gender = '';
+		}
+		this.setState({...this.state, error});
+	}
+
 	render() {
 		return (
-			<Form>
-				<InputText id="firstName" placeholder="Nome" value={this.state.firstName} onChange={this.setInputTextValue}/>
-				<InputText id="lastName" placeholder="Sobrenome" value={this.state.lastName} onChange={this.setInputTextValue}/>
+			<FormControl>
+				<TextField id="firstName" label="Nome" required={true} value={this.state.firstName} onChange={this.setInputTextValue}  error={this.state.error.firstName !== ''} helperText={this.state.error.firstName}/>
+				<TextField id="lastName" label="Sobrenome" required={true} value={this.state.lastName} onChange={this.setInputTextValue} error={this.state.error.lastName !== ''} helperText={this.state.error.lastName}/>
+				<FormControl>
+					<InputLabel htmlFor="gender">Sexo</InputLabel>
+					<Select
+						native
+						value={this.state.gender}
+						onChange={this.setInputTextValue}
+						inputProps={{
+							name: 'gender',
+							id: 'gender',
+						}}
+					>
+						<option aria-label="None" value="" />
+						<option value={'M'}>Masculino</option>
+						<option value={'F'}>Feminino</option>
+					</Select>
+					<FormHelperText error={this.state.error.gender !== ''}>{this.state.error.gender}</FormHelperText>
+				</FormControl>
+				<Button variant="contained" color="primary" onClick={this.validate}>
+					Salvar
+				</Button>
 
-				<label htmlFor="gender">Sexo</label>
-				<select name="gender" id="genders">
-					<option value="Masculino">Masculino</option>
-					<option value="Feminino">Feminino</option>
-				</select>
-			</Form>
+			</FormControl>
 		);
 	}
 }
